318c4b2a4bde6d0dfa7a0e288ed4c58bd13a5552
joycecsilveira <joycesilveiracos@gmail.com>
2021-12-17 14:14:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+    const [tasks, setTasks]= useState([
+      {
+        id:'1',
+        title: 'Estudar programação',
+        completed: false,
+      },
+      {  
+        id: "2",
+        title: "Ler livros",
+        completed: true,
+      },
+    ]);
+
+    return (
+
+    )
+
+  export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Este arquivo renderiza o app através do root. 
+// < /> caracteriza os componentes do REACT.
+
+
