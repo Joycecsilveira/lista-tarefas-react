767910f47a485197d2f0540027851d7b7997167e
Joyce <joycesilveiracos@gmail.com>
2021-12-22 09:56:08 -0300

    procfile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-// Este arquivo renderiza o app através do root. 
+// Este arquivo renderiza o app através do root.
 // < /> caracteriza os componentes do REACT.
-
-
